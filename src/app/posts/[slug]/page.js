28edbd835a98168f3b7ace5ee31143bd95b5c82e
Gabriel Reynoso <garey91@gmail.com>
2025-08-27 23:52:03 +0200

    feat: enhance BlogPost component to display publication date and improve layout

--- src/app/posts/[slug]/page.js
+++ src/app/posts/[slug]/page.js
@@ -3,23 +3,20 @@ import { StoryblokStory } from "@storyblok/react/rsc";
 import getSbVersion from "@/utils/getSbVersion";
 
 export default async function BlogPost({ params }) {
-  const slug = params.slug;
+  const { slug } = await params;
 
   const { data } = await fetchData(slug);
 
   return (
     <div className="page">
-      <StoryblokStory story={data.story} />
+      <StoryblokStory story={data.story} date={data.story.published_at} />
     </div>
   );
 }
 
 export async function fetchData(slug) {
   const storyblokApi = getStoryblokApi();
-  return (
-    await storyblokApi.get(`cdn/stories/posts/${slug}`),
-    {
-      version: getSbVersion(),
-    }
-  );
+  return await storyblokApi.get(`cdn/stories/posts/${slug}`, {
+    version: getSbVersion(),
+  });
 }
